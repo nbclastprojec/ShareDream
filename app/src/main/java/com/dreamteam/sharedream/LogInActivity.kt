da1450eb04190f55a38b0e22bb5a8a7d7822590a
june <dydrkfl078@naver.com>
2023-10-17 21:30:49 +0900

    [Feature] Firebase ui를 사용하지 않고 auth 데이터 추가 구현(test)

--- app/src/main/java/com/dreamteam/sharedream/LogInActivity.kt
+++ app/src/main/java/com/dreamteam/sharedream/LogInActivity.kt
@@ -2,10 +2,23 @@ package com.dreamteam.sharedream
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.dreamteam.sharedream.databinding.ActivityLogInBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LogInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLogInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.login_frame_layout,LoginFragment())
+                .commit()
+        }
     }
 }
--- app/src/main/java/com/dreamteam/sharedream/LoginFragment.kt
+++ app/src/main/java/com/dreamteam/sharedream/LoginFragment.kt
@@ -1,59 +1,77 @@
 package com.dreamteam.sharedream
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.dreamteam.sharedream.databinding.FragmentLoginBinding
+import com.dreamteam.sharedream.databinding.FragmentLoginMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        auth = Firebase.auth
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Fragment_login.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogin.setOnClickListener {
+            val signInEmail = binding.editEmail.text.toString()
+            val signInPassword = binding.editPassword.text.toString()
+            if (signInEmail.contains("@") && signInEmail.contains(".com")) {
+                createAccount(signInEmail, signInPassword)
+                val intent = Intent(activity, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(requireContext(), "이메일 형식이 올바르지 않습니다.", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private fun createAccount(email: String, password: String) {
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth?.createUserWithEmailAndPassword(email, password)
+                ?.addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(requireContext(), "계정 생성 완료", Toast.LENGTH_SHORT).show()
+
+                    } else {
+                        Toast.makeText(requireContext(), "계정 생성 실패", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        var currentUser = auth.currentUser
     }
 }
