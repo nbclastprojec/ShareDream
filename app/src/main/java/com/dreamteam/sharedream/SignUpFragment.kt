017c8c538cdcdfcb9a143582023a882b6b52dadc
1231234qr312 <137616115+1231234qr312@users.noreply.github.com>
2023-10-18 20:11:21 +0900

    Update SignUpFragment.kt
    회원가입화면수정

--- app/src/main/java/com/dreamteam/sharedream/SignUpFragment.kt
+++ app/src/main/java/com/dreamteam/sharedream/SignUpFragment.kt
@@ -13,67 +13,71 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 
-class LoginFragment : Fragment() {
-    private lateinit var auth:FirebaseAuth
-    private lateinit var binding:FragmentLoginBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        auth = FirebaseAuth.getInstance()
+class SignUpFragment : Fragment() {
+    private lateinit var binding:FragmentSignupBinding
+    private lateinit var auth: FirebaseAuth
 
 
 
 
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentLoginBinding.inflate(inflater, container, false)
-
-        binding.findPassword.setOnClickListener {
-            val findPasswordFragment=FindPasswordFragment()
-            val transaction=requireActivity().supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment_container,findPasswordFragment)
-            transaction.addToBackStack(null)
-            transaction.commit()
-        }
-
-        binding.tvFindID.setOnClickListener {
-            val findIdFragment=FindIdFragment()
-            val transaction=requireActivity().supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment_container,findIdFragment)
-            transaction.addToBackStack(null)
-            transaction.commit()
-        }
+        binding= FragmentSignupBinding.inflate(inflater,container,false)
+        auth = FirebaseAuth.getInstance() //
 
 
-        binding.btnLogin.setOnClickListener {
-            val homeFragment= HomeFragment()
-            val transaction=requireActivity().supportFragmentManager.beginTransaction()
-            val email = binding.editEmail.text.toString()
-            val password= binding.editPassword.text.toString()
-            if (check()) {
-                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        val intent = Intent(activity, MainActivity::class.java)
-                        startActivity(intent)
-                    } else {
 
-                        Toast.makeText(requireContext(), "로그인 실패: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+        binding.btnSignup.setOnClickListener {
+            val email=binding.editEmail.text.toString()
+            val password=binding.editPassword.text.toString()
+            val nickname=binding.nickname.text.toString()
+            val number=binding.editPhoneNumber.text.toString()
+            val user=auth.currentUser
+            val uid=user?.uid
+            val firestore=FirebaseFirestore.getInstance()
+            val userCollection=firestore.collection("UserData")
+            val userDocument = userCollection.document(uid?:"vCi5OvWi8tj91J6mQRV0")
+            if(check()){
+                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
+                    if(it.isSuccessful){
+
+
+
+                        val userData= hashMapOf(
+                            "email" to email,
+                            "number" to number,
+                            "nickName" to nickname
+                        )
+
+                        userDocument.set(userData).addOnSuccessListener {
+                            Toast.makeText(requireContext(),"회원가입 성공",Toast.LENGTH_SHORT).show()
+
+
+
+                        }
+                            .addOnFailureListener { e->
+
+                                Toast.makeText(requireContext(),"회원가입 실패",Toast.LENGTH_SHORT).show()
+
+                            }
+
+
                     }
                 }
-            }
 
+            }
         }
         return binding.root
     }
 
-
     private fun check():Boolean {
         val email = binding.editEmail.text.toString()
         val password = binding.editPassword.text.toString()
+        val passwordCk=binding.editPasswordCheck.text.toString()
+        val number=binding.editPhoneNumber.text.toString()
         if (email.isEmpty()) {
             binding.editEmail.error = "이메일을 입력해주세요."
             return false
@@ -88,7 +92,22 @@ class LoginFragment : Fragment() {
         } else if (!special(password)) {
             binding.editPassword.error = "최소 하나 이상의 특수문자를 입력해 주세요."
             return false
+        } else if (password!=passwordCk){
+            binding.editPasswordCheck.error="비밀번호와 똑같이 입력해 주세요."
+            return false
+
+        } else if (!phone(number)){
+            binding.editPassword.error="핸드폰 번호는 숫자로만 이루어져야 합니다."
+            return false
+
         } else {
+            Toast.makeText(requireContext(), "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+            auth.signOut()
+            val loginFragment=LoginFragment()
+            val transaction=requireActivity().supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container,loginFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
 
             return true
 
@@ -102,6 +121,9 @@ class LoginFragment : Fragment() {
         val specialWord= setOf('!','@','#','$','%','^','&','*','(',')','_','-','<','>','=','+','?')//특수문자
         return text.any{it in specialWord//비밀번호 중 하나(any)라도 문자 안에 특수문자(speacialWord)가 포함되어있으면 true반환
         }
-    }
 
+    }
+    fun phone(text: String):Boolean{
+        return text.all {it.isDigit()}
+    }
 }
