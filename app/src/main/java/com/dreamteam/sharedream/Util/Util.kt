772480a76537dc5e4077acd4b9c34226d2763723
june <dydrkfl078@naver.com>
2023-11-08 16:06:09 +0900

    [hotfix]게시물 작성, 수정 완료 버튼 중복 방지 처리

--- app/src/main/java/com/dreamteam/sharedream/Util/Util.kt
+++ app/src/main/java/com/dreamteam/sharedream/Util/Util.kt
@@ -52,6 +52,25 @@ object Util {
 
 }
 
+class DuplicateClickListener(private val click: (View) -> Unit) : View.OnClickListener {
+
+    companion object {
+        private const val CLICK_INTERVAL = 1000
+    }
+
+    private var lastClickedTime: Long = 0L
+
+    override fun onClick(v: View?) {
+        if (isSafe() && v != null) {
+            lastClickedTime = System.currentTimeMillis()
+            click(v)
+        }
+    }
+
+    private fun isSafe(): Boolean {
+        return System.currentTimeMillis() - lastClickedTime > CLICK_INTERVAL
+    }
+}
 
 object ToastMsg{
     var toast: Toast? = null
